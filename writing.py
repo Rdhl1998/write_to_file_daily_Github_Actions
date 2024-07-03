82f7e66097caa10d57d09f25730aa0a19287e959
Rdhl1998 <140719916+Rdhl1998@users.noreply.github.com>
2024-07-03 16:39:31 +0200

    Create writing.py
    Added the main functionalities to the file

---
+++ writing.py
@@ -0,0 +1,11 @@
+'''
+Created: 2024/07/03 
+last modified: 2024/07/03 
+Purpose of the file is to daily append the current day to the --> dates.txt file
+The format will be  DD-MM-YYYY HH:MM:SS /n
+'''
+
+import datetime
+x = datetime.datetime.now()
+
+print(x)
